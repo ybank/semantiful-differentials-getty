8f6c05473d377396ba8756fafae1995f366bfebd
Y. Yan <dev@ybank.net>
2016-01-26 23:08:12 -0800

    #SEMADIFF-17 Resolved

--- scripts/Center.py
+++ scripts/Center.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
+import ast
 import subprocess
 
 # TODO: git rev-parse HEAD
 
 def sys_call(cmd):
-    ret = subprocess.call(cmd)
+    ret = subprocess.call(cmd.split(" "))
     if ret != 0:
-        print "Error in command: " + " ".join(cmd)
+        print "Error in command: " + cmd
         raise SystemExit("system exit: " + str(ret))
+
+def read_str_from(file_path):
+    with open(file_path, 'r') as file:
+        str_ds = file.read()
+    return ast.literal_eval(str_ds)
 
 
--- src/Villa.java
+++ src/Villa.java
@@ -1,5 +1,7 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -12,7 +14,6 @@ import edu.ucsd.getty.comp.ASTInspector;
 import edu.ucsd.getty.comp.CandidateGenerator;
 import edu.ucsd.getty.comp.InputDiffProcessor;
 import edu.ucsd.getty.diff.GitDiff;
-import edu.ucsd.getty.diff.Patch;
 
 public class Villa {
 
@@ -35,49 +36,182 @@ public class Villa {
 		String package_prefix = null;
 		String prev_commit = null;
 		String curr_commit = null;
+		String old_srcpath = null;
+		String output_dir = null;
 		
-		if (args.length == 6) {
-			diff_path = args[0];
-			target_path = args[1];
-			test_path = args[2];
-			package_prefix = args[3];
-			prev_commit = args[4];
-			curr_commit = args[5];
-		} else {
-			System.out.println("Incorrect number of arguments given");
+		if (args.length < 1 || args[0].equals("-h") || args[0].equals("--help")) {
+			print_help_info();
 			System.exit(1);
 		}
 		
-		try {
-			System.out.println("Parsing differential file ...\n");
-			IInputProcessor diff_processor = new InputDiffProcessor();
-			GitDiff git_diff = diff_processor.parseDiff(diff_path, prev_commit, curr_commit);
-			Map<String, Integer[]> file_revision_lines = diff_processor.newLinesRevised(git_diff);
-			
-			System.out.println("\nGetting changed methods (in .java files only, excluding tests) ...\n");
-			IMethodRecognizer ast_inspector = new ASTInspector();
-			Set<String> exclusion = new HashSet<String>();
-			for (String file : file_revision_lines.keySet())
-				if (!file.endsWith(".java") || file.startsWith(test_path))
-					exclusion.add(file);
-			for (String ext : exclusion)
-				file_revision_lines.remove(ext);
-				
-			Set<String> revised_methods = ast_inspector.changedMethods(file_revision_lines);
-			System.out.println("changed methods: " + revised_methods + "\n");
-			
-			System.out.println("\nGetting call graphs and candidate call chains ...\n");
-			ITraceFinder chain_generator = new CandidateGenerator(revised_methods, target_path, package_prefix);
-			Map<String, Set<List<String>>> candidates = chain_generator.getCandidateTraces();
-			
-			// TODO: output for filter
-			System.out.println(candidates);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(2);
+		switch(args[0]) {
+			case "-s":
+			case "--simgen":
+				if (args.length != 7 && args.length != 9) {
+					System.out.println("Incorrect (number of) arguments given");
+					print_help_info();
+					System.exit(1);
+				}
+				diff_path = args[1];
+				target_path = args[2];
+				test_path = args[3];
+				package_prefix = args[4];
+				prev_commit = args[5];
+				curr_commit = args[6];
+				if (args.length == 9 && (args[7].equals("-o") || args[7].equals("--output"))) {
+					output_dir = args[8];
+					if(!output_dir.endsWith("/"))
+						output_dir += "/";
+				} else {
+					output_dir = "/tmp/getty/";
+				}
+				try {
+					Map<String, Integer[]> file_revision_lines = get_revised_file_lines_map(diff_path, prev_commit, curr_commit);
+					
+					Set<String> revised_methods = get_changed_methods(test_path, file_revision_lines);
+//					System.out.println("changed methods: " + revised_methods + "\n");
+					String chgmtd_out_path = output_dir + "_getty_chgmtd_src_" + prev_commit + "_" + curr_commit + "_.ex";
+					System.out.println("number of changed methods: " + revised_methods.size() + "\n"
+							+ "  output to file " + chgmtd_out_path + " ...\n");
+					output_to(chgmtd_out_path, revised_methods);
+					
+					Map<String, Set<List<String>>> candidates = get_candidates(target_path, package_prefix, revised_methods);
+//					System.out.println(candidates);
+					String ccc_out_path = output_dir + "_getty_ccc_" + curr_commit + "_.ex";
+					System.out.println("size of ccc map: " + candidates.size() + "\n"
+							+ "  output to file " + ccc_out_path + " ...\n");
+					output_to(ccc_out_path, candidates);
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+					System.exit(2);
+				}
+				break;
+			case "-c":
+			case "--comgen":
+				if (args.length != 8 && args.length != 10) {
+					System.out.println("Incorrect (number of) arguments given");
+					print_help_info();
+					System.exit(1);
+				}
+				diff_path = args[1];
+				target_path = args[2];
+				test_path = args[3];
+				package_prefix = args[4];
+				prev_commit = args[5];
+				curr_commit = args[6];
+				old_srcpath = args[7];
+				if (args.length == 10 && (args[8].equals("-o") || args[8].equals("--output"))) {
+					output_dir = args[9];
+					if(!output_dir.endsWith("/"))
+						output_dir += "/";
+				} else {
+					output_dir = "/tmp/getty/";
+				}
+				try {
+					// FIXME: more accurate changed methods
+					
+					Map<String, Integer[]> old_file_revision_lines = get_original_file_lines_map(diff_path, prev_commit, curr_commit);
+					Map<String, Integer[]> file_revision_lines = get_revised_file_lines_map(diff_path, prev_commit, curr_commit);
+					
+					// TODO: more steps ...
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+					System.exit(2);
+				}
+				break;
+			default:
+				System.out.println("Incorrect (number of) arguments given");
+				System.exit(1);
+				break;
 		}
 		
+	}
+
+	private static void output_to(String out_path, Set<String> set_content) throws IOException {
+		PrintWriter out_file = new PrintWriter(
+				new BufferedWriter(new FileWriter(out_path, false)));
+		String str_content = "[";
+		for (String method : set_content) {
+			str_content += ("\"" + method + "\", ");
+		}
+		str_content += "]";
+		out_file.print(str_content);
+		out_file.close();
+	}
+	
+	private static void output_to(String out_path, Map<String, Set<List<String>>> ccc_content) throws IOException {
+		PrintWriter out_file = new PrintWriter(
+				new BufferedWriter(new FileWriter(out_path, false)));
+		String str_content = "{";
+		for (String method : ccc_content.keySet()) {
+			str_content += ("\"" + method + "\": [");
+			for (List<String> chain : ccc_content.get(method)) {
+				str_content += "[";
+				for (String mtd : chain) {
+					str_content += ("\"" + mtd + "\", ");
+				}
+				str_content += "], ";
+			}
+			str_content += "], ";
+		}
+		str_content += "}";
+		out_file.print(str_content);
+		out_file.close();
+	}
+
+	private static void print_help_info() {
+		System.out.println("Usage:"
+				+ "\n\t  "
+				+ "java -jar Getty.Villa.jar "
+				+ "--simgen|-s diffpath targetpath testpath pkgprefix prevcommit currcommit "
+				+ "[--output|-o outputworkdir]"
+				+ "\n\t  "
+				+ "java -jar Getty.Villa.jar "
+				+ "--comgen|-c diffpath targetpath testpath pkgprefix prevcommit currcommit oldbinpath "
+				+ "[--output|-o outputworkdir]"
+				+ "\n");
+	}
+
+	private static Map<String, Set<List<String>>> get_candidates(String target_path, String package_prefix,
+			Set<String> revised_methods) {
+		System.out.println("\nGetting call graphs and candidate call chains ...\n");
+		ITraceFinder chain_generator = new CandidateGenerator(revised_methods, target_path, package_prefix);
+		Map<String, Set<List<String>>> candidates = chain_generator.getCandidateTraces();
+		return candidates;
+	}
+
+	private static Set<String> get_changed_methods(String test_path, Map<String, Integer[]> file_revision_lines) {
+		System.out.println("\nGetting changed methods (in .java files only, excluding tests) ...\n");
+		IMethodRecognizer ast_inspector = new ASTInspector();
+		Set<String> exclusion = new HashSet<String>();
+		for (String file : file_revision_lines.keySet())
+			if (!file.endsWith(".java") || file.startsWith(test_path))
+				exclusion.add(file);
+		for (String ext : exclusion)
+			file_revision_lines.remove(ext);
+			
+		Set<String> revised_methods = ast_inspector.changedMethods(file_revision_lines);
+		return revised_methods;
+	}
+
+	private static Map<String, Integer[]> get_revised_file_lines_map(String diff_path, String prev_commit, String curr_commit)
+			throws Exception {
+		System.out.println("Parsing differential file for the revised ...\n");
+		IInputProcessor diff_processor = new InputDiffProcessor();
+		GitDiff git_diff = diff_processor.parseDiff(diff_path, prev_commit, curr_commit);
+		Map<String, Integer[]> file_revision_lines = diff_processor.newLinesRevised(git_diff);
+		return file_revision_lines;
+	}
+	
+	private static Map<String, Integer[]> get_original_file_lines_map(String diff_path, String prev_commit, String curr_commit)
+			throws Exception {
+		System.out.println("Parsing differential file for the original ...\n");
+		IInputProcessor diff_processor = new InputDiffProcessor();
+		GitDiff git_diff = diff_processor.parseDiff(diff_path, prev_commit, curr_commit);
+		Map<String, Integer[]> file_revision_lines = diff_processor.oldLinesRevised(git_diff);
+		return file_revision_lines;
 	}
 
 }
