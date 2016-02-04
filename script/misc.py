b4d0b7b4209abf9e0ab0ef3fb594c0d647dc8d69
Y. Yan <dev@ybank.net>
2016-02-04 05:05:37 -0800

    environment adjustment

---
+++ script/misc.py
@@ -0,0 +1,68 @@
+# misc: utils for analysis, algorithms, prefix processings, etc.
+
+
+# Return the longest prefix of all list elements. The return might be short.
+def __common_prefix(m):
+    if not m:
+        raise ValueError('list of strings should not be empty')
+    min_s = min(m)
+    max_s = max(m)
+    for i, c in enumerate(min_s):
+        if c != max_s[i]:
+            return min_s[:i]
+    return min_s
+
+
+# Return common prefix
+__min_common_prefix_length = 10  # minimum length set, magical number
+def common_prefixes(m):
+    if not m:
+        raise ValueError('list of strings should not be empty')
+    result = set()
+    current_prefix = ''
+    working = []
+    remaining = list(m)
+    remaining.sort()
+    simple_result = __common_prefix(m)
+    if len(simple_result) > __min_common_prefix_length:
+        return [simple_result]
+    else:
+        while remaining != []:
+            temp = remaining.pop()
+            if working == []:
+                working.append(temp)
+                current_prefix = temp
+            else:  # current_prefix is based on working, and is long enough
+                if not temp.startswith(current_prefix):
+                    working.append(temp)
+                    new_prefix = __common_prefix(working)
+                    if len(new_prefix) > __min_common_prefix_length:
+                        current_prefix = new_prefix
+                    else:
+                        result.add(current_prefix)
+                        working = [temp]
+                        current_prefix = temp
+        if working == []:
+            raise RuntimeError('right after loop working set should not be empty')
+        else:
+            result.add(current_prefix)
+    return result
+
+
+# formalize method (etc.) prefixes so they are recognizable by Daikon filter
+def formalize(prefixes):
+    result = []
+    for prefix in prefixes:
+        prefix = prefix.replace(":", ".").replace(".", "\.")
+        result.append(prefix)
+    return "\|".join(result)
+
+
+# formalize one method so it is recognizable by Daikon filter
+def dfformat(method):
+    return method.replace(":", ".").replace(".", "\.") + "\("
+
+
+# formalize one method so it is recognizable by Daikon filter
+def fsformat(method):
+    return method.replace(":", "_").replace("$", "_").replace(".", "_")
---
+++ script/villa.py
@@ -0,0 +1,90 @@
+from utils import *
+
+
+def visit(villa_path, pwd, go, prev_hash, post_hash, pkg_prefix="-"):
+    
+    print("\n*************************************************************");
+    print("Getty Villa: Semantiful Differential Analyzer");
+    print("*************************************************************\n");
+    
+    print "current working directory: " + pwd + "\n"
+    
+    diff_out = go + "text.diff"
+    sys_call("git diff {0} {1} > {2}".format(prev_hash, post_hash, diff_out))
+    
+    '''
+        1-st pass: checkout prev_commit as detached head, and get all sets and etc, in simple mode (-so)
+            remember to clear after this pass
+    '''
+    sys_call("git checkout " + prev_hash)
+    
+    # src_path = path_from_mvn_call("sourceDirectory")
+    bin_path = path_from_mvn_call("outputDirectory")
+    test_src_rel_path = path_from_mvn_call("testSourceDirectory")
+    if test_src_rel_path.startswith(pwd):
+        test_src_rel_path = test_src_rel_path[len(pwd):]
+    else:
+        raise ValueError("pwd is not a prefix of test src path")
+    print "current test src path (relative): " + test_src_rel_path + "\n"
+    
+    sys_call("mvn test -DskipTests")
+    
+    run_villa = "java -jar {0} -so {1} {2} {3} {4} {5} {6} -o {7}".format(
+        villa_path, diff_out, bin_path, test_src_rel_path, pkg_prefix, prev_hash, post_hash, go)
+    print "\n\nstart to run Villa ... \n\n" + run_villa
+    sys_call(run_villa)
+    
+    old_changed_methods = read_str_from(go + "_getty_chgmtd_src_old_{0}_.ex".format(prev_hash))
+    old_all_callers = read_str_from(go + "_getty_clr_{0}_.ex".format(prev_hash))
+    old_all_cccs = read_str_from(go + "_getty_ccc_{0}_.ex".format(prev_hash))
+    old_all_methods = read_str_from(go + "_getty_allmtd_src_{0}_.ex".format(prev_hash))
+#     # DEBUG ONLY
+#     print old_changed_methods
+#     print old_all_callers
+#     print old_all_cccs
+#     print old_all_methods
+    
+    clear_temp_checkout(prev_hash)
+    
+    '''
+        2-nd pass: checkout post_commit as detached head, and get all sets and etc, in complex mode (-c)
+            remember to clear after this pass
+    '''
+    sys_call("git checkout " + post_hash)
+    
+    # src_path = path_from_mvn_call("sourceDirectory")
+    bin_path = path_from_mvn_call("outputDirectory")
+    test_src_rel_path = path_from_mvn_call("testSourceDirectory")
+    if test_src_rel_path.startswith(pwd):
+        test_src_rel_path = test_src_rel_path[len(pwd):]
+    else:
+        raise ValueError("pwd is not a prefix of test src path")
+    print "current test src path (relative): " + test_src_rel_path + "\n"
+    
+    sys_call("mvn test -DskipTests")
+    
+    run_villa = "java -jar {0} -c {1} {2} {3} {4} {5} {6} -o {7}".format(
+        villa_path, diff_out, bin_path, test_src_rel_path, pkg_prefix, prev_hash, post_hash, go)
+    print "\n\nstart to run Villa ... \n\n" + run_villa
+    sys_call(run_villa)
+    
+    new_changed_methods = read_str_from(go + "_getty_chgmtd_src_new_{0}_.ex".format(post_hash))
+    new_improved_changed_methods = read_str_from(go + "_getty_chgmtd_src_{0}_{1}_.ex".format(prev_hash, post_hash))
+    new_removed_changed_methods = read_str_from(go + "_getty_chgmtd_src_gone_{0}_{1}_.ex".format(prev_hash, post_hash))
+    new_all_callers = read_str_from(go + "_getty_clr_{0}_.ex".format(post_hash))
+    new_all_cccs = read_str_from(go + "_getty_ccc_{0}_.ex".format(post_hash))
+    new_all_methods = read_str_from(go + "_getty_allmtd_src_{0}_.ex".format(post_hash))
+#     # DEBUG ONLY
+#     print new_changed_methods
+#     print new_improved_changed_methods
+#     print new_removed_changed_methods
+#     print new_all_callers
+#     print new_all_cccs
+#     print new_all_methods
+    
+    clear_temp_checkout(post_hash)
+    
+    print 'Villa analysis is completed.'
+    return old_changed_methods, old_all_callers, old_all_cccs, old_all_methods, \
+        new_changed_methods, new_improved_changed_methods, new_removed_changed_methods, \
+        new_all_callers, new_all_cccs, new_all_methods
