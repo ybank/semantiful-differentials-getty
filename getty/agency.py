7790ea46973367d2c00ed1769f48b7e29fb4d555
Y. Yan <dev@ybank.net>
2016-03-09 06:41:52 -0800

    #SEMADIFF-45: Pending review

---
+++ getty/agency.py
@@ -0,0 +1,6 @@
+# get interested runtime targets
+
+
+# TODO: get targets by two passes
+def investigate(go, agent_path, old_all_methods, new_all_methods, prev_hash, post_hash):
+    pass
--- getty/tools/ex.py
+++ getty/tools/ex.py
@@ -7,3 +7,8 @@ def read_str_from(file_path):
     with open(file_path, 'r') as file:
         str_ds = file.read()
     return ast.literal_eval(str_ds)
+
+
+def save_to(file, data):
+    with open(file, 'w') as f:
+        f.write(data)
